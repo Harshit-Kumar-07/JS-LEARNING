fb651c20fc966b3b029bb5e1081a571997b8dca0
Harshit-Kumar-07 <horakumar0707@gmail.com>
2026-07-26 22:24:03 +0530

    completed day-1

---
+++ 01_BASICS/03_conversion_operations.js
@@ -0,0 +1,53 @@
+// let score = "47ad" // NaN means not a number
+// score = null
+// score = undefined
+// score = true
+// score = false
+// score = "47"
+
+
+// console.log(typeof(score))
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber));
+// console.log(typeof valueInNumber)
+
+// console.log(valueInNumber)
+
+let isValid = 1
+let boolValid = Boolean(isValid)
+
+// console.log(boolValid)
+
+let str = "hora"
+let boolstr = Boolean(str)
+// console.log(boolstr);
+// console.log(typeof boolstr);
+
+// ***************** OPERATIONS *****************
+
+let val = 7
+// let negval = -val
+// console.log(negval);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(17%4);
+// console.log(2/3);
+
+let str1 = "hello"
+let str2 = " Harshit"
+let summation = str1 + str2
+console.log(summation);
+
+let counter = 0;
+console.log(counter++);
+console.log(++counter);
+
+
+
+
