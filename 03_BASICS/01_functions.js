f908a6c6ce9e449bfb771e9dbfd2c1635cad1281
Harshit-Kumar-07 <horakumar0707@gmail.com>
2026-07-31 20:30:59 +0530

    learning scope

--- 03_BASICS/01_functions.js
+++ 03_BASICS/01_functions.js
@@ -1,7 +1,52 @@
-function add(num1,num2){
-    return num1  + num2;
+// // // // function add(num1,num2){
+// // // //     return num1  + num2;
+// // // // }
+
+// // // // let result = add(7,9) 
+
+// // // // console.log(result);
+
+
+
+// // // function loginUserMessage(username){
+// // //     if(username === undefined){
+// // //         console.log("please enter a username");
+// // //         return;
+// // //     }
+// // //     console.log( `${username} just logged in` )
+// // // }
+
+// // // loginUserMessage("Harshit");
+
+
+// // // rest operator
+// // function calculateCartPrice(n1,n2,...num1){
+// //     return num1
+// // }
+
+// // console.log(calculateCartPrice(20,40,700,800));
+
+
+
+
+// const user = {
+//     username : "harshit",
+//     price : 299
+// }
+
+// handleObject(user)
+
+// function handleObject(objectname){
+//     console.log(`name is ${objectname.username} and price is ${objectname.price}`);
+        
+// }
+
+
+
+const arr = [3,4,5,6,9]
+
+function printele(arr){
+    return arr[4]
 }
 
-let result = add(7,9) 
-
-console.log(result);
+console.log(printele(arr));
---
+++ 03_BASICS/02_scopes.js
@@ -0,0 +1,43 @@
+// let a = 20
+// const b = 30
+
+// if(10===10){
+//     let c = 80
+//     a = 40
+//     console.log(a);
+    
+// }
+
+// console.log(a);
+// console.log(c);
+
+
+if(true){
+    const username = "harshit"
+    if(username ==="harshit"){
+        const website = "youtube"
+        console.log(username + website)        
+    }
+   // console.log(website)    
+}
+//console.log(username);
+
+
+
+
+
+
+
+//----------------------- INTERESTING -----------------------
+
+
+addone(5)
+function addone(n){
+    return n+1
+}
+
+
+addTwo(7) // cannot access function before declaration in this type of funciton declaration
+const addTwo = function(num){
+    return num+2
+}
