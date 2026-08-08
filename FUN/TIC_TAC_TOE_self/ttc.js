d458fe28f0aa519f6849a11610ce0b7832d1e5b5
Harshit-Kumar-07 <horakumar0707@gmail.com>
2026-08-08 13:59:28 +0530

    game tie message add

--- FUN/TIC_TAC_TOE_self/ttc.js
+++ FUN/TIC_TAC_TOE_self/ttc.js
@@ -1,6 +1,10 @@
 let player1=true;
         let player2=false;
 
+        let counter = 0
+        let Xwon = false;
+        let Owon = false;
+
         let matrix = [['','',''],['','',''],['','','']]
         function isANS(matrix){
             if( matrix[0][0]=='X' && matrix[0][1]=='X' && matrix[0][2]=='X')return 'X';
@@ -44,12 +48,19 @@ let player1=true;
                 if(player1 ){                    
                     e.target.innerHTML = 'X'
                     matrix[row][col] = 'X'
+                    counter++;
                     if(isANS(matrix)=='X'){
                         document.getElementById('message').innerHTML = 'Player X WON'
+                        Xwon = true;
                         return;
                     }
                     player1=false;
                     player2=true;
+                     // Check for tie
+        if (counter == 9) {
+            document.getElementById('message').innerHTML = "It's a tie";
+            return;
+        }
                     document.getElementById('message').innerHTML = 'Player O turn'
                 }
                 
@@ -57,13 +68,22 @@ let player1=true;
                 else if(player2){                    
                     e.target.innerHTML = 'O'
                     matrix[row][col] = 'O'
+                    counter++;
                     if(isANS(matrix)=='O'){
                         document.getElementById('message').innerHTML = 'Player O WON'
+                        Owon = true;
                         return;
                     }
                     player1=true;
                     player2=false;  
+                     // Check for tie
+        if (counter == 9) {
+            document.getElementById('message').innerHTML = "It's a tie";
+            return;
+        }
                     document.getElementById('message').innerHTML = 'Player X turn' 
                 }
 
         })
+
+        
