116d82faa1e135abfc4c0d7cfd82972bd754fcff
Harshit-Kumar-07 <horakumar0707@gmail.com>
2026-08-22 10:41:11 +0530

    added theme changer button in the weather app

--- WEATHER_APP/script.js
+++ WEATHER_APP/script.js
@@ -31,3 +31,21 @@ async function checkWeather(city){
 searchIcon.addEventListener('click',function(){
     checkWeather(searchBox.value)
 })
+
+
+let theme = "light"
+
+
+document.getElementById('theme')
+.addEventListener('click',function(){
+    if(theme=="light"){
+        document.getElementById('card').style.background = 'linear-gradient(90deg, #121216 0.000%, #121216 7.692%, #16171a calc(7.692% + 1px), #16171a 15.385%, #1a1b1f calc(15.385% + 1px), #1a1b1f 23.077%, #1f2023 calc(23.077% + 1px), #1f2023 30.769%, #232427 calc(30.769% + 1px), #232427 38.462%, #27282b calc(38.462% + 1px), #27282b 46.154%, #2b2c2f calc(46.154% + 1px), #2b2c2f 53.846%, #2f3033 calc(53.846% + 1px), #2f3033 61.538%, #323337 calc(61.538% + 1px), #323337 69.231%, #36373a calc(69.231% + 1px), #36373a 76.923%, #38393d calc(76.923% + 1px), #38393d 84.615%, #3b3c40 calc(84.615% + 1px), #3b3c40 92.308%, #3d3e42 calc(92.308% + 1px) 100.000%)'
+        document.getElementById('theme').innerHTML = '<img src="IMAGES/sun.png">'
+        theme = "dark"
+    }
+    else {
+        document.getElementById('card').style.background = 'linear-gradient(45deg, #4159d0 0.000%, #c84fc0 50.000%, #ffcd70 100.000%)'
+        document.getElementById('theme').innerHTML = '<img src="IMAGES/moon.png">'
+        theme = "light"
+    }
+})
